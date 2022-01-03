4671d1575f119e05aba8139c3ffe5a57b862861d
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-01-03 12:48:04 +0900

    asd

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
     if (input.value !== "") {
       const div = document.createElement("div");
       document.body.appendChild(div);
-      document.body.appendChild(div);
 
       const checkbox = document.createElement("input");
-
       checkbox.type = "checkbox";
+      checkbox.id = "checkbox";
       checkbox.addEventListener("change", () => {
         if (checkbox.checked) {
           div.style.textDecoration = "line-through";
@@ -15,7 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
           div.style.textDecoration = "";
         }
       });
-
       div.appendChild(checkbox);
 
       const span = document.createElement("span");
@@ -23,27 +21,39 @@ document.addEventListener("DOMContentLoaded", () => {
       input.value = "";
       div.appendChild(span);
 
-      const deleteButton = document.createElement("button");
-      deleteButton.textContent = "삭제";
+      const today = new Date();
 
+      const p = document.createElement("p");
+      p.textContent =
+        today.getHours() +
+        "시" +
+        today.getMinutes() +
+        "분" +
+        today.getSeconds() +
+        "초";
+      input.value = "";
+      div.appendChild(p);
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "제거하기";
       deleteButton.addEventListener("click", () => {
         div.parentNode.removeChild(div);
       });
       div.appendChild(deleteButton);
     }
-    today = new Date();
-    document.getElementById("va2").innerHTML =
-      today.getHours() +
-      "시 " +
-      today.getMinutes() +
-      "분 " +
-      today.getSeconds() +
-      "초 ";
   };
+
+  const title = document.createElement("div");
+  document.body.appendChild(title);
+  title.id = "testID";
 
   const h1 = document.createElement("h1");
   h1.textContent = "할 일 목록";
-  document.body.appendChild(h1);
+
+  title.appendChild(h1);
+
+  const form = document.createElement("div");
+  document.body.appendChild(form);
 
   const input = document.createElement("input");
   input.addEventListener("keyup", (event) => {
@@ -51,13 +61,12 @@ document.addEventListener("DOMContentLoaded", () => {
       addTodo();
     }
   });
-
-  document.body.appendChild(input);
+  form.appendChild(input);
 
   const addButton = document.createElement("button");
   addButton.textContent = "추가하기";
+  form.appendChild(addButton);
 
-  document.body.appendChild(addButton);
   addButton.addEventListener("click", () => {
     if (input.value !== "") {
       addTodo();
