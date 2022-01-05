88a25bdeffe3f269ab5ba2412de5f828880c4a35
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-01-05 18:10:37 +0900

    dwqdas

--- main.js
+++ main.js
@@ -1,80 +1,117 @@
-const inputVal = document.getElementsByClassName("inputVal")[0];
-
-const addTaskBtn = document.getElementsByClassName("btn")[0];
-
-const today = new Date();
-
-function add() {
-  if (inputVal.value.trim() != 0) {
-    let localItems = JSON.parse(localStorage.getItem("localItem"));
+document.addEventListener("DOMContentLoaded", () => {
+  const addTodo = () => {
+    let localItems = JSON.parse(localStorage.getItem("애플리케이션2"));
     if (localItems === null) {
       taskList = [];
     } else {
       taskList = localItems;
     }
-    taskList.push(inputVal.value);
-    localStorage.setItem("localItem", JSON.stringify(taskList));
-    input.value = "";
-  }
+    taskList.push(input.value);
+    localStorage.setItem("애플리케이션2", JSON.stringify(taskList));
+    if (input.value !== "") {
+      const div = document.createElement("div");
+      document.body.appendChild(div);
 
-  showItem();
-}
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.id = "checkbox";
 
-addTaskBtn.addEventListener("click", function () {
-  if (inputVal.value.trim() != 0) {
-    let localItems = JSON.parse(localStorage.getItem("localItem"));
-    if (localItems === null) {
-      taskList = [];
-    } else {
-      taskList = localItems;
+      checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+          div.style.textDecoration = "line-through";
+        } else {
+          div.style.textDecoration = "";
+        }
+      });
+      div.appendChild(checkbox);
+
+      let toDos = [];
+      const toDoInput = document.querySelector("div");
+
+      const span = document.createElement("span");
+      span.textContent = input.value;
+      input.value = "";
+      div.appendChild(span);
+
+      const today = new Date();
+
+      const p = document.createElement("h6");
+      p.textContent = today.getHours() + ":" + today.getMinutes();
+      input.value = "";
+      div.appendChild(p);
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "제거하기";
+      deleteButton.addEventListener("click", () => {
+        div.parentNode.removeChild(div);
+        // localStorage.clear();
+        // 저장된 녀석을 모두 제거
+      });
+      div.appendChild(deleteButton);
     }
-    taskList.push(inputVal.value);
-    localStorage.setItem("localItem", JSON.stringify(taskList));
-  }
+  };
 
-  showItem();
-});
+  const title = document.createElement("div");
+  document.body.appendChild(title);
+  title.id = "testID";
 
-function showItem() {
-  let localItems = JSON.parse(localStorage.getItem("localItem"));
-  if (localItems === null) {
-    taskList = [];
-  } else {
-    taskList = localItems;
-  }
+  const h1 = document.createElement("h1");
+  h1.textContent = "할 일 목록";
 
-  let html = "";
-  let itemShow = document.querySelector(".todoLists");
+  title.appendChild(h1);
 
-  taskList.forEach((data, index) => {
-    html += `
-    <div id="line" class="todoList">
-    <input type="checkbox" id="checkbox">
-    <h6 class="date">${today.getHours() + ":" + today.getMinutes()}</h6>
-    <p class="pText">${data}</p>
-    <button class="deleteTask" onClick="deleteItem(${index})">제거하기</button>
-    </div>
-    `;
+  const form = document.createElement("div");
+  document.body.appendChild(form);
+
+  const input = document.createElement("input");
+
+  input.addEventListener("keyup", (event) => {
+    input.addEventListener("keyup", () => {
+      // 입력 양식 내부의 값을 저장
+    });
+    if (event.keyCode === 13) {
+      addTodo();
+    }
   });
-  itemShow.innerHTML = html;
-}
-showItem();
+  form.appendChild(input);
 
-function deleteItem(index) {
-  let localItems = JSON.parse(localStorage.getItem("localItem"));
-  taskList.splice(index, 1);
-  localStorage.setItem("localItem", JSON.stringify(taskList));
-  showItem();
-}
+  const addButton = document.createElement("button");
+  addButton.textContent = "추가하기";
+  form.appendChild(addButton);
 
-function clearTask() {
-  localStorage.clear();
-  showItem();
-}
+  addButton.addEventListener("click", () => {
+    if (input.value !== "") {
+      addTodo();
+    }
+  });
+  const load = () => {
+    const data = localStorage.getItem("애플리케이션");
+    if (data !== null) {
+      return JSON.parse(data);
+    } else {
+      return {
+        message: "",
+        id: "",
+      };
+    }
+  };
+  const save = (data) => {
+    localStorage.setItem("애플리케이션", JSON.stringify(data));
+  };
 
-const input = document.getElementsByClassName("inputVal")[0];
-input.addEventListener("keyup", (event) => {
-  if (event.keyCode === 13) {
-    add();
-  }
+  const data = load();
+  input.value = data.message;
+
+  input.addEventListener("keyup", () => {
+    data.message = input.value;
+    data.id = Date.now();
+    save(data);
+  });
 });
+
+// json
+// 키는 문자열만!
+// 문자열은 반드시 큰따옴표!
+// 이외에도 데이터는 숫자, 문자열, 불만 저장할 수 있다.
+// 컨테이너 데이터는 객체, 배열만 사용할 수 있다 등이 있습니다.
+// 함수를 저장하거나 할 수는 없습니다.
