14a7b3705fbf7a9b6e72fdd76cf759335538e30e
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-01-10 12:59:59 +0900

    asdsadsad

---
+++ todo.js
@@ -0,0 +1,95 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function filterFn(toDo) {
+  return toDo.id === 1;
+}
+
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = "checkbox";
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      li.style.textDecoration = "line-through";
+    } else {
+      li.style.textDecoration = "";
+    }
+  });
+
+  let today = new Date();
+
+  let time = document.createElement("h6");
+  time.textContent =
+    String(today.getHours()) + ":" + String(today.getMinutes());
+
+  const li = document.createElement("li");
+  li.id = "listing";
+
+  const delBtn = document.createElement("button");
+  delBtn.id = "box1";
+  const span = document.createElement("span");
+  span.id = "box1";
+  const newId = toDos.length + 1;
+  delBtn.innerHTML = "제거하기";
+  delBtn.addEventListener("click", deleteToDo);
+  span.innerText = text;
+  li.appendChild(time);
+  li.appendChild(checkbox);
+
+  li.appendChild(span);
+  li.appendChild(delBtn);
+
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
--- weather.js
+++ weather.js
@@ -1,69 +1,23 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "aa6ceff75ee966bc45ddbfba4edf90b1";
-// http://api.openweathermap.org/data/2.5/weather?lat=37.56826&lon=126.977829&APPID=${API_KEY}
 
-const api = {
-  key: "aa6ceff75ee966bc45ddbfba4edf90b1",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
+function getWeather() {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${API_KEY}`
+  )
+    .then(function (respone) {
+      return respone.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      weather.innerText = `서울의 현재 온도는  ${Math.ceil(
+        temperature - 273.15
+      )}도`;
+    });
+}
 
-const dateBuilder = (d) => {
-  let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  // sunday 먼저..!!
-  let days = ["Sun", "Mon", "Tue", "Wed", "Tur", "Fri", "Sat"];
-
-  let day = days[d.getDay()];
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-  let date = d.getDate();
-
-  return `${day} ${date} ${month} ${year}`;
-};
-
-const city = "Seoul";
-const url = `${api.base}weather?q=${city}&appid=${api.key}`;
-const [weather, setWeather] = useState("");
-
-axios.get(url).then((responseData) => {
-  const data = responseData.data;
-  setWeather({
-    id: data.weather[0].id,
-    temperature: data.main.temp,
-    main: data.weather[0].main,
-    loading: false,
-  });
-});
-
-let c = weather.temperature - 273.15;
-
-const selectIcon = () => {
-  let iconId = weather.id === 800 ? 0 : (parseInt(weather.id) / 100).toFixed(0);
-  switch (iconId) {
-    case "0":
-      return <TiWeatherSunny size="6rem" color="red" />;
-    case "2":
-      return <TiWeatherStormy size="6rem" color="black" />;
-    case "3":
-      return <TiWeatherShower size="6rem" color="blue" />;
-    case "5":
-      return <TiWeatherDownpour size="6rem" color="navy" />;
-    case "6":
-      return <TiWeatherSnow size="6rem" color="white" />;
-    case "7":
-      return <BsCloudFog size="6rem" color="white" />;
-    case "8":
-      return <TiWeatherCloudy size="6rem" color="white" />;
-  }
-};
+function init() {
+  getWeather();
+}
+init();
